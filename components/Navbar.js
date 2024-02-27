d9886f9fae2c08ed5cc9dd369abb7b526797e6e3
mdshakilkhan841 <mdshakilkhan841@gmail.com>
2024-02-28 01:57:08 +0600

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="bg-gray-800 h-12 p-2 text-white flex items-center">
+      Navbar
+    </div>
+  );
+}
---
+++ components/ProductCard.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Product() {
+    return(
+        <div className="border">
+            <Image />
+        </div>
+    )
+};
